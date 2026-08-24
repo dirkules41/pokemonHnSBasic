c339eafaa14c498539231eb286f668008f10d78b
dirkules41 <max.liebler.psn@gmail.com>
2026-08-25 00:10:43 +0200

    Change shiny Totodile determination logic

--- src/starter_choose.c
+++ src/starter_choose.c
@@ -757,7 +757,7 @@ static u8 CreatePokemonFrontSpriteTotodile(u16 species, u8 x, u8 y)
     static u32 isShinyTotodile = 0;
 
     if (isShinyTotodile == 0)
-        isShinyTotodile = (1);
+        isShinyTotodile = (Random()% 128);
 
     if ((isShinyTotodile < SHINY_ODDS) && (gSaveBlock1Ptr->tx_Features_ShinyChance == 0)) // 1/8192
     {
